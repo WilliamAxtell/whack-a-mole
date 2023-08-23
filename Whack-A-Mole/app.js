9205b924d7f8a3609f663fc3b441902ab60056e3
WilliamAxtell <111174139+WilliamAxtell@users.noreply.github.com>
2023-08-23 17:05:05 +0100

    JS work

--- Whack-A-Mole/app.js
+++ Whack-A-Mole/app.js
@@ -5,13 +5,26 @@ const score = document.querySelector('#score');
 
 let result = 0;
 
-function randomSqure() {
+function randomSquare() {
     squares.forEach(square => {
         square.classList.remove("mole");
     })
 
     let randomSquare = squares[Math.floor(Math.random() * 9)];
     randomSquare.classList.add("mole");
+
+    hitPosition = 
 }
 
-randomSqure();
+squares.forEach(square => {
+    square.addEventListener('mousedown' () => {
+        if (square.id == )
+    })
+})
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 500);
+}
+
+moveMole();
