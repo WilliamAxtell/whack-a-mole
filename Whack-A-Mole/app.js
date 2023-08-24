e787055d3551e16a9e7e60f4b6d99e9b69836127
WilliamAxtell <111174139+WilliamAxtell@users.noreply.github.com>
2023-08-24 12:46:05 +0100

    Final version

--- Whack-A-Mole/app.js
+++ Whack-A-Mole/app.js
@@ -4,6 +4,9 @@ const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
 
 let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
 
 function randomSquare() {
     squares.forEach(square => {
@@ -13,18 +16,34 @@ function randomSquare() {
     let randomSquare = squares[Math.floor(Math.random() * 9)];
     randomSquare.classList.add("mole");
 
-    hitPosition = 
+    hitPosition = randomSquare.id;
 }
 
 squares.forEach(square => {
-    square.addEventListener('mousedown' () => {
-        if (square.id == )
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++;
+            score.innerHTML = result;
+            hitPosition = null;
+        }
     })
 })
 
 function moveMole() {
-    let timerId = null;
     timerId = setInterval(randomSquare, 500);
 }
 
 moveMole();
+
+function countDown() {
+    currentTime--;
+    timeLeft.innerHTML = currentTime;
+
+    if(currentTime == 0) {
+       clearInterval(countDownTimerId);
+       clearInterval(timerId);
+       alert('GAME OVER! Your final score is ' + result); 
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
